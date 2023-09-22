4d4360379c20faae7fedc43394f2eff00346d3d7
aneduardaa <142325462+aneduardaa@users.noreply.github.com>
2023-09-22 00:04:08 -0300

    Create atividade5.c

---
+++ atividade5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+ int numero = 1, soma = 0;
+
+ while (numero <= 50){
+    if (numero % 2 == 0){
+        soma += numero;
+    }
+
+
+ }
+printf("\na soma dos primeiros 50 pares e:%d", soma);
+
+
+
+}
